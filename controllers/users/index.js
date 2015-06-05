65022bd3480848206fb5c8823185646124d106cc
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2015-06-05 15:33:14 -0300

    feat(api): add /users endpoint
    This end point provides all users registered in the app.

---
+++ controllers/users/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+var UsersModel = require('../../models/users');
+
+module.exports = function (router) {
+    var users = new UsersModel();
+
+    router.get('/', function(req, res) {
+        res.status(200);
+        res.send(users);
+    });
+};
