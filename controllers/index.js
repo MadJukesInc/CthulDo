de5aaa5c2a628324a6392cf1bd1e6955410fcdff
pineauk <kpineau@upei.ca>
2015-06-08 14:26:39 -0300

    chore(index.js): make root html file server

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,18 @@
 'use strict';
 
 
-var IndexModel = require('../models/index');
 
 
 module.exports = function (router) {
 
-    var model = new IndexModel();
 
 
     router.get('/', function (req, res) {
-        
-        
-        res.render('index', model);
-        
-        
+
+
+        res.render('layouts/master', {});
+
+
     });
 
 };
