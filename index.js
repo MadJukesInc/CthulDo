124f136b8b1048eb53d68f42267fc21a00f32ffd
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2015-06-08 13:38:42 -0300

    feat(conf): place conf loader into separate file

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
+
 var app = require('express')();
 var kraken = require('kraken-js');
 
+var options = require('./lib/conf');
 
 var debugPort = process.env.PORT || 8000;
 var httpPort = 80;
@@ -13,28 +15,13 @@ var httpsPort = 443;
 var debugServer;
 var httpServer;
 var httpsServer;
-var options;
 
 var httpsOptions = {
     key: fs.readFileSync('./server.key'),
     cert: fs.readFileSync('./server.crt')
 };
 
-/*
- * Create and configure application. Also exports application instance for use by tests.
- * See https://github.com/krakenjs/kraken-js#options for additional configuration options.
- */
-options = {
-    onconfig: function (config, next) {
-        /*
-         * Add any additional config setup or overrides here. `config` is an initialized
-         * `confit` (https://github.com/krakenjs/confit/) configuration object.
-         */
-        next(null, config);
-    }
-};
-
-app.use(kraken(options));
+app.use(kraken(options(app)));
 app.on('start', function () {
     console.log('Application ready to serve requests.');
     console.log('Environment: %s', app.kraken.get('env:env'));
