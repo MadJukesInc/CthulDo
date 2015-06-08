bac3c2da05997cfb0e14d0d6c43930deac831dfd
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2015-06-08 10:38:44 -0300

    feat(index): add debug, http and https servers
    These three servers are for the same application. They server on three
    different ports and with different protocols.

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 'use strict';
 
+var fs = require('fs');
 var http = require('http');
+var https = require('https');
 var app = require('express')();
 var kraken = require('kraken-js');
 
 
-var server;
+var debugPort = process.env.PORT || 8000;
+var httpPort = 80;
+var httpsPort = 443;
+var debugServer;
+var httpServer;
+var httpsServer;
 var options;
+
+var httpsOptions = {
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.crt')
+};
 
 /*
  * Create and configure application. Also exports application instance for use by tests.
@@ -28,8 +40,17 @@ app.on('start', function () {
     console.log('Environment: %s', app.kraken.get('env:env'));
 });
 
-server = http.createServer(app);
-server.listen(process.env.PORT || 8000);
-server.on('listening', function () {
+debugServer = http.createServer(app).listen(debugPort);
+debugServer.on('listening', function () {
+    console.log('Server listening on http://localhost:%d', this.address().port);
+});
+
+httpServer = http.createServer(app).listen(httpPort);
+httpServer.on('listening', function () {
+    console.log('Server listening on http://localhost:%d', this.address().port);
+});
+
+httpsServer = https.createServer(httpsOptions, app).listen(httpsPort);
+httpsServer.on('listening', function () {
     console.log('Server listening on http://localhost:%d', this.address().port);
 });
