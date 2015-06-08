4cdbe9056fa3e22836f402ed8215f19b84372c59
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2015-06-08 16:50:23 -0300

    feat(Tasks): tie tasks public model to DB

--- models/tasks.js
+++ models/tasks.js
@@ -1,22 +1,70 @@
 'use strict';
 
+var _ = require('lodash');
+
+var Tasks = require('../lib/dbModels/Tasks');
+
 var TasksModel = function TasksModelConstructor() {
-    return [{
-        title: 'one',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        completed: false,
-        members: [],
-        owner: 'me'
-    }, {
-        title: 'two',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        completed: true,
-        members: [],
-        owner: 'me'
-    }];
+    return {
+        /**
+         * Find tasks and execute callback
+         * @param {Number} [id] ID of the task
+         * @param {Function} cb Callback to execute after we found tasks
+         * @returns {*}
+         */
+        get: function (id, cb) {
+            if (_.isUndefined(cb) && _.isFunction(id)) {
+                cb = id;
+                id = null;
+            }
+
+            if (_.isNull(id)) {
+                return Tasks.all()
+                    .then(function (results) {
+                        cb(null, results);
+                    })
+                    .catch(function (err) {
+                        cb(err);
+                    });
+            }
+            else {
+                return Tasks.findOne({ where: { id: id } })
+                    .then(function (results) {
+                        cb(null, results);
+                    })
+                    .catch(function (err) {
+                        cb(err);
+                    });
+            }
+        },
+        post: function (task, cb) {
+            return Tasks.create(task)
+                .then(function (err, result) {
+                    if (err) {
+                        cb(err);
+                        return;
+                    }
+
+                    cb(null, result);
+                })
+                .catch(function (err) {
+                    cb(err);
+                });
+        },
+        put: function (id, task, cb) {
+            return Tasks.update(task, { where: { id: id } })
+                .then(function () {
+                    console.log(arguments);
+                })
+        },
+        delete: function (id, cb) {
+            return Tasks.destroy({ where: { id: id } })
+                .then(function () {
+                    console.log(arguments);
+                })
+        }
+    }
 };
 
 
-module.exports = TasksModel;
+module.exports = TasksModel();
