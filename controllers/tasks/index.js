bd6342518240eece7240853b3e42d958e4b51119
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2015-06-09 15:20:39 -0300

    feat(controllers/tasks): add REST calls for /tasks/:taskID endpoint
    Added REST calls are GET, POST, PUT, and DELETE.

--- controllers/tasks/index.js
+++ controllers/tasks/index.js
@@ -5,6 +5,21 @@ var tasks = require('../../models/tasks');
 
 
 module.exports = function (router) {
+    var onTaskPostOrPut = function(req, res) {
+        var taskID = req.params.taskID;
+        var task = req.body;
+
+        tasks.put(taskID, task, function (err, results) {
+            if (err) {
+                res.sendStatus(500);
+                throw err;
+            }
+
+            res.status(200);
+            res.send(results);
+        });
+    };
+
     router.get('/', function (req, res) {
         tasks.get(function (err, results) {
             if (err) {
@@ -32,18 +47,34 @@ module.exports = function (router) {
     });
 
     router.get('/:taskID', function(req, res) {
+        var taskID = req.params.taskID;
 
+        tasks.get(taskID, function (err, results) {
+            if (err) {
+                res.sendStatus(500);
+                throw err;
+            }
+
+            res.status(200);
+            res.send(results);
+        })
     });
 
-    router.post('/:taskID', function(req, res) {
+    router.post('/:taskID', onTaskPostOrPut);
 
-    });
-
-    router.put('/:taskID', function(req, res) {
-
-    });
+    router.put('/:taskID', onTaskPostOrPut);
 
     router.delete('/:taskID', function(req, res) {
+        var taskID = req.params.taskID;
 
+        tasks.delete(taskID, function(err, results) {
+            if (err) {
+                res.sendStatus(500);
+                throw err;
+            }
+
+            res.status(200);
+            res.send(results);
+        });
     });
 };
