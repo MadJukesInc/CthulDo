77302b318a431f357f14b80995838a9a45a0c9d1
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2015-06-09 10:43:43 -0300

    feat(controllers/users): update api for new model

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,11 +1,32 @@
 'use strict';
 
 var _ = require('lodash');
-var Users = require('../../models/users');
+var users = require('../../models/users');
 
 module.exports = function (router) {
     router.get('/', function(req, res) {
-        res.status(200);
-        res.send(users);
+        users.get(function(err, results) {
+            if (err) {
+                res.sendStatus(500);
+                throw err;
+            }
+
+            res.status(200);
+            res.send(results);
+        });
+    });
+
+    router.post('/', function(req, res) {
+        var newUser = req.body;
+
+        users.post(newUser, function(err, results) {
+            if (err) {
+                res.sendStatus(500);
+                throw err;
+            }
+
+            res.status(200);
+            res.send(results);
+        });
     });
 };
