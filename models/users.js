fb8ab74677e51cbba2bc078c58e707428f796ac8
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2015-06-09 10:58:47 -0300

    feat(models/users): allow fetching info about one user

--- models/users.js
+++ models/users.js
@@ -1,11 +1,27 @@
 'use strict';
 
+var _ = require('lodash');
+
 var Users = require('../lib/dbModels/Users');
 
 var UsersModel = function UsersModelConstructor() {
     return {
-        get: function(cb) {
-            return Users.all()
+        get: function(name, cb) {
+            var found;
+
+            if (_.isUndefined(cb) && _.isFunction(name)) {
+                cb = name;
+                name = null;
+            }
+
+            if (_.isNull(name)) {
+                found = Users.all();
+            }
+            else {
+                found = Users.findOne({ where: { name: name } });
+            }
+
+            return found
                 .then(function (result) {
                     cb(null, result);
                 })
