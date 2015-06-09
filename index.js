16027b4e2e505789aab541f38aba524a9428869e
pineauk <kpineau@upei.ca>
2015-06-09 16:07:03 -0300

    feat(index.js): add authentication modules to express

--- index.js
+++ index.js
@@ -2,10 +2,36 @@
 
 var app = require('express')();
 var kraken = require('kraken-js');
+var passport = require('passport');
+var auth = require('./lib/auth');
+var flash = require('connect-flash');
+var session = require('express-session')
+//var UserModel = require('./models/Users');
 
 var options = require('./lib/conf');
 
-app.use(kraken(options(app)));
+passport.use(new auth.localStrategy());
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    var user = {
+        id: 'test',
+        username: 'bob',
+        password: 'bob',
+        role: 'admin'
+    };
+    done(null, user);
+});
+
+app.use(kraken(options(app)))
+    .use(session({secret: 'keyboard cat'}))
+    .use(passport.initialize())
+    .use(passport.session())
+    .use(flash())
+    .use(auth.injectUser);
+
 app.on('start', function () {
     console.log('Application ready to serve requests.');
     console.log('Environment: %s', app.kraken.get('env:env'));
