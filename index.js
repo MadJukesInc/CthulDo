6e727cae29f47264158bd3d278f85b54303ba0cf
pineauk <kpineau@upei.ca>
2015-06-09 23:20:59 -0300

    fix(index.js): remove problemed imports

--- index.js
+++ index.js
@@ -2,12 +2,6 @@
 
 var app = require('express')();
 var kraken = require('kraken-js');
-var passport = require('passport');
-var auth = require('./lib/auth');
-var flash = require('connect-flash');
-var session = require('express-session')
-//var UserModel = require('./models/Users');
-
 
 var options = require('./lib/conf');
 
