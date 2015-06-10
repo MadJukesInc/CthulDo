27e2439b3c7488af1354133881a0bbc3e2b7db5e
pineauk <kpineau@upei.ca>
2015-06-10 18:22:22 -0300

    feat(/api/tasks): add some publish rules, not very secure atm

--- controllers/api/tasks/index.js
+++ controllers/api/tasks/index.js
@@ -5,23 +5,17 @@ var tasks = require('../../../models/tasks');
 
 
 module.exports = function (router) {
-    var onTaskPostOrPut = function(req, res) {
+    var onTaskPostOrPut = function (req, res) {
         var taskID = req.params.taskID || req.body.id;
         var task = req.body;
-        task.owner = req.user.id;
-        tasks.put(taskID, task, function (err, results) {
-            if (err) {
-                res.sendStatus(500);
-                throw err;
-            }
-
-            res.status(200);
-            res.send(results);
-        });
-    };
-    router.put('/', onTaskPostOrPut);
-    router.get('/', function (req, res) {
-            tasks.get(function (err, results) {
+        if (_.isUndefined(task.owner)) {
+            task.owner = req.user.id;
+        }
+        else if (task.owner !== req.user.id) {
+            res.sendStatus(401);
+        }
+        else {
+            tasks.put(taskID, task, function (err, results) {
                 if (err) {
                     res.sendStatus(500);
                     throw err;
@@ -30,10 +24,24 @@ module.exports = function (router) {
                 res.status(200);
                 res.send(results);
             });
+        }
+    };
+    router.put('/', onTaskPostOrPut);
+    router.get('/', function (req, res) {
+        var user = req.user;
+        tasks.get(user,function (err, results) {
+            if (err) {
+                res.sendStatus(500);
+                throw err;
+            }
+
+            res.status(200);
+            res.send(results);
+        });
 
     });
 
-    router.post('/', function(req, res) {
+    router.post('/', function (req, res) {
         var newTask = req.body;
         newTask.owner = req.user.id;
         tasks.post(newTask, function (err, results) {
@@ -47,10 +55,10 @@ module.exports = function (router) {
         });
     });
 
-    router.get('/:taskID', function(req, res) {
+    router.get('/:taskID', function (req, res) {
         var taskID = req.params.taskID;
-
-        tasks.get(taskID, function (err, results) {
+        var user = req.user;
+        tasks.get(taskID,user, function (err, results) {
             if (err) {
                 res.sendStatus(500);
                 throw err;
@@ -65,16 +73,27 @@ module.exports = function (router) {
 
     router.put('/:taskID', onTaskPostOrPut);
 
-    router.delete('/:id', function(req, res) {
+    router.delete('/:id', function (req, res) {
         var taskID = req.params.id;
-        tasks.delete(taskID, function(err, results) {
-            if (err) {
-                res.sendStatus(500);
-                throw err;
+        tasks.get(taskID, function (err, task) {
+            if (_.isUndefined(task.owner)) {
+                task.owner = req.user.id;
             }
+            else if (task.owner !== req.user.id) {
+                res.sendStatus(401);
+            }
+            else {
+                tasks.delete(taskID, function (err, results) {
+                    if (err) {
+                        res.sendStatus(500);
+                        throw err;
+                    }
 
-            res.status(200);
-            res.send(results);
+                    res.status(200);
+                    res.send(results);
+                });
+            }
         });
+
     });
 };
